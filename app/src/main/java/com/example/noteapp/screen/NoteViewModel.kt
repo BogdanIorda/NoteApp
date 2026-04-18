249e3a6ce02242ea06050e0afb1a1475c5e1e873
Bogdan <iordachescubogdann@gmail.com>
2026-04-18 13:22:38 +0300

    Refactor: Convert Settings cold Flow to hot StateFlow

--- app/src/main/java/com/example/noteapp/screen/NoteViewModel.kt
+++ app/src/main/java/com/example/noteapp/screen/NoteViewModel.kt
@@ -8,13 +8,18 @@ import com.example.noteapp.repository.NoteRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.WhileSubscribed
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class NoteViewModel @Inject constructor(private val repository: NoteRepository) : ViewModel() {
+
+    /*
     private val _noteList = MutableStateFlow<List<Note>>(emptyList())
     val noteList = _noteList.asStateFlow()
 
@@ -25,6 +30,15 @@ class NoteViewModel @Inject constructor(private val repository: NoteRepository) 
             }
         }
     }
+     */
+
+    val noteList = repository.getAllNotes()
+        .distinctUntilChanged()
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = emptyList()
+        )
 
     fun addNote(note: Note) = viewModelScope.launch { repository.addNote(note) }
     fun updateNote(note: Note) = viewModelScope.launch { repository.updateNote(note) }
